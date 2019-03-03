dedae60ae438a3a429c527abb7f7d73d92b5ecc1
Alex Ku <alex.ku96@outlook.com>
2019-03-03 13:28:28 -0500

    Add project files.

---
+++ LanguageFeatures/Models/myExtensionMethods.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace LanguageFeatures.Models
+{
+    public static class myExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productParam)
+        {
+            decimal total = 0; 
+            foreach(Product prod in productParam)
+            {
+                total += prod.Price;
+            }
+            return total;
+        }
+        public static IEnumerable<Product> FilterByCategory(
+        this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+        public static IEnumerable<Product> Filter(
+        this IEnumerable<Product> productEnum, Func<Product, bool> selectorParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (selectorParam(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+    }
+
+  
+}
